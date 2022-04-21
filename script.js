92f16c4c9273f82d67a7de99decc9f98400e11c6
Guilherme Raposo <gui.raposo36@gmail.com>
2022-04-21 10:35:33 -0300

    feat: finished application

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const timeDisplay = document.getElementById("time");
 const day = document.getElementById("day");
+const img = document.getElementById("image");
 const now = new Date();
 const hour = now.getHours();
 
@@ -15,10 +16,14 @@ setInterval(refreshTime, 1000);
 
 if ((hour >= 6) & (hour <= 12)) {
   day.innerHTML = "Bom Dia !";
+  img.src = "./assets/morning.jpg";
 } else if ((hour >= 12) & (hour <= 18)) {
   day.innerHTML = "Boa Tarde !";
+  img.src = "./assets/afternoon.jpg";
 } else if ((hour >= 19) & (hour <= 24)) {
   day.innerHTML = "Boa Noite !";
+  img.src = "./assets/night.jpg";
 } else {
   day.innerHTML = "Boa Madrugada !";
+  img.src = "./assets/dawn.jpg";
 }
